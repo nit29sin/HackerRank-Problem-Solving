4c26ec77a2012c96bf28c2b960d64594a0e843c1
nit29sin <59767668+nit29sin@users.noreply.github.com>
2020-01-15 12:29:01 +0530

    Update Solve Me First.cpp

--- Solve Me First.cpp
+++ Solve Me First.cpp
@@ -23,3 +23,28 @@ Explanation
 
 The sum of the two integers  and  is computed as: .
 */
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int s;
+  s=a+b;
+  return s;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
+
